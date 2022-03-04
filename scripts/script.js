7716eec8ee1630a19270423b734cf6f2f2b4c301
Nivaldo Farias <badivia.f@gmail.com>
2022-03-04 20:34:04 -0300

    Added first screen functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const API_KEY = "42b5ff12a2084bc029141577acd4bc26";
 
-let geolocation
+let geolocation;
 let lat;
 let lon;
 let text = document.getElementById("text");
@@ -16,8 +16,7 @@ function geolocationAvailable() {
 
 function getLocation() {
   if (navigator.geolocation) {
-    geolocation =
-    navigator.geolocation.getCurrentPosition(showPosition);
+    geolocation = navigator.geolocation.getCurrentPosition(showPosition);
   } else {
     text.innerHTML = "Geolocation is not supported by this browser.";
   }
@@ -35,7 +34,7 @@ function showPosition(position) {
     position.coords.latitude +
     "<br>Longitude: " +
     position.coords.longitude;
-    getWeatherDataLatLon(position)
+  getWeatherDataLatLon(position);
 }
 
 geolocationAvailable();
@@ -43,30 +42,40 @@ InitializeBtn();
 
 function InitializeBtn() {
   const geolocationBtn = document.querySelector("#data-btn");
+  const adressBtn = document.querySelector("#alt-btn");
+  const altBtn = document.querySelector("#decline-btn");
+
   geolocationBtn.addEventListener("click", () => {
     getLocation();
     geolocationBtn.classList.add("clicked");
   });
-  const adressBtn = document.querySelector("#alt-btn");
   adressBtn.addEventListener("click", () => {
     getWeatherDataAdress();
     adressBtn.classList.add("clicked");
   });
+  altBtn.addEventListener("click", () => {
+    altBtn.classList.add("hidden");
+    geolocationBtn.classList.add("hidden");
+    document.querySelector("#first-screen > p").classList.add("hidden");
+    document.querySelector("section").classList.remove("hidden");
+  });
 }
 
-
-
-function getWeatherDataLatLon(position){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`)
-  .then((response) => response.json())
-  .then(data => console.log(data))
+function getWeatherDataLatLon(position) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
-function getWeatherDataAdress(){
-  const city = document.querySelector("#city").value
-  const state = document.querySelector("#state").value
-  const country = document.querySelector("#country").value
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${API_KEY}`)
-  .then((response) => response.json())
-  .then(data => console.log(data))
+function getWeatherDataAdress() {
+  const city = document.querySelector("#city").value;
+  const state = document.querySelector("#state").value;
+  const country = document.querySelector("#country").value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
